b0236590c407baa271951cb15aac74bffcd6deb6
Richard Holmes <richard.gordon.holmes@gmail.com>
2019-03-24 11:50:39 +0000

    Added module for creating and reading entity strings (unfinished)

---
+++ PlatformerGameEngine/EntityStringHandler.vb
@@ -0,0 +1,15 @@
+﻿'Richard Holmes
+'24/03/2019
+'Entity file handler
+
+Imports PRE2 = PlatformerGameEngine.PanelRenderEngine2
+
+Module EntityStringHandler
+
+    Public Function CreateEntityString(ent As PRE2.Entity) As String
+        'creates a string which can be saved to a file
+
+
+    End Function
+
+End Module
