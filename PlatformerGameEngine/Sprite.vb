92133d08be7683fb57e8337d1d3225a4d74fb2fb
Richard Holmes <richard.gordon.holmes@gmail.com>
2019-11-12 06:45:30 +0000

    Updated Sprite class, now contains New and ToString procedures itself

--- PlatformerGameEngine/Sprite.vb
+++ PlatformerGameEngine/Sprite.vb
@@ -1,4 +1,6 @@
-﻿Public Class Sprite
+﻿Imports PRE2 = PlatformerGameEngine.PanelRenderEngine2
+
+Public Class Sprite
     'sprites loaded from .sprt files and used to create frames
 
     Private pixelColours(,) As Color
@@ -6,35 +8,10 @@
 
     Private bitmapVersion As Image
 
-    Public Sub New(fileLocation As String)
-        'creates a new sprite by reading it from a file
-
-        If IO.File.Exists(fileLocation) Then
-            Dim reader As New IO.StreamReader(fileLocation)
-            Dim fileText As String = reader.ReadToEnd
-
-            reader.Close()
-
-            If SpriteStringHandler.ValidSpriteText(fileText, fileLocation) Then
-                Pixels = SpriteStringHandler.ReadPixelColours(fileText)
-            End If
-
-            'FindSpriteFileName(fileLocation)
-            fileName = fileLocation
-        Else
-            PanelRenderEngine2.DisplayError("Couldn't find file " & fileLocation)
-        End If
-    End Sub
-
-    'Public Sub New(spriteString As String, fileLocation As String)
-    '    'creates a new sprite from a string
-
-    '    If SpriteHandler.ValidSpriteText(spriteString, fileLocation) Then
-    '        pixels = SpriteHandler.ReadPixelColours(spriteString)
-    '    End If
-
-    '    fileName = fileLocation
-    'End Sub
+    Private Const spriteTagName As String = "sprite"
+    Private Const fileTagName As String = "fileName"
+    Private Const coloursTagName As String = "colours"
+    Private Const pixelsTagName As String = "pixels"
 
     Public Sub New(colourIndices(,) As Integer, colours() As Color)
         'uses colours and colour indices to make a sprite
@@ -51,37 +28,105 @@
         Next x
     End Sub
 
-    Public Sub FindSpriteFileName(fileLocation As String)
-        'finds the extra part of the file location, relative to the sprites folder
-        'eg D:\Users\Example\sprites\mario\MarioIdle.sprt -> \mario\marioIdle.sprt
+    Public Overrides Function ToString() As String
+        'converts this sprite to a tag with subtags {file name, colours, colour indices}
+        Dim colourIndices()() As Integer = Nothing
+        Dim colourNames() As String = Nothing
+        If Not IsNothing(Pixels) Then
+            'finds all colours used
+            Dim coloursUsed() As Color = {Color.Transparent}
+            colourNames = {ColorTranslator.ToHtml(coloursUsed(0))}
+            For index2 As Integer = 0 To Pixels.GetUpperBound(1)
+                For index1 As Integer = 0 To Pixels.GetUpperBound(0)
+                    'checks if colour has already been added
+                    Dim duplicate As Boolean = False
+                    For colourIndex As Integer = 0 To UBound(coloursUsed)
+                        If Pixels(index1, index2) = coloursUsed(colourIndex) Then
+                            duplicate = True
+                            Exit For
+                        End If
+                    Next
 
-        Dim spriteFolderName As String = "sprites"
-        Dim folders() As String = fileLocation.Split("\")
-        fileName = ""
+                    'only adds colour if it isn't a duplicate
+                    If Not duplicate Then
+                        coloursUsed = InsertItem(coloursUsed, Pixels(index1, index2))
+                        colourNames = InsertItem(colourNames, ColorTranslator.ToHtml(coloursUsed(UBound(coloursUsed))))
+                    End If
+                Next
+            Next
 
-        If folders.Contains(spriteFolderName) = True Then
-            Dim foundFolder As Boolean = False
+            'finds the colour index of each pixel
+            ReDim colourIndices(Pixels.GetUpperBound(0))
+            For index1 As Integer = 0 To Pixels.GetUpperBound(0)
+                ReDim colourIndices(index1)(Pixels.GetUpperBound(1))
+                For index2 As Integer = 0 To Pixels.GetUpperBound(1)
+                    For colourIndex As Integer = 0 To UBound(coloursUsed)
+                        If coloursUsed(colourIndex) = Pixels(index1, index2) Then
+                            colourIndices(index1)(index2) = colourIndex
+                        End If
+                    Next
+                Next
+            Next
+        End If
 
-            For index As Integer = 0 To UBound(folders)
-                If foundFolder = True Then
-                    fileName += "\" & folders(index)
-                End If
+        Return New Tag(spriteTagName, ArrayToString({
+                    New Tag(fileTagName, fileName),
+                    New Tag(coloursTagName, ArrayToString(colourNames)),
+                    New Tag(pixelsTagName, ArrayToString(colourIndices))
+                                               })).ToString
+    End Function
 
-                If folders(index) = spriteFolderName Then
-                    foundFolder = True
-                End If
-            Next index
-        Else
-            PanelRenderEngine2.DisplayError("Sprite at " & fileLocation & " is not in folder " & spriteFolderName)
+    Public Sub New(ByVal fileLocation As String, ByVal spriteFolderLocation As String)
+        Dim fileText As String = PanelRenderEngine2.ReadFile(fileLocation)
+        If Not IsNothing(fileText) Then
+            fileName = fileLocation.Remove(0, Len(spriteFolderLocation))
+            Dim spriteTag As New Tag(fileText)
+
+            Dim newSprite As New Sprite(spriteTag.ToString)
+            Pixels = newSprite.Pixels
         End If
     End Sub
 
-    Public Overrides Function ToString() As String
-        Return SpriteStringHandler.CreateString(Me)
-    End Function
+    Public Sub New(ByVal spriteString As String)
+        Dim spriteTag As New Tag(spriteString)
+        Dim coloursTag As New Tag(spriteTag.InterpretArgument(coloursTagName))
+        Dim pixelsTag As New Tag(spriteTag.InterpretArgument(pixelsTagName))
+
+        If Not IsNothing(coloursTag) Then
+            Dim colourNames() As String = coloursTag.InterpretArgument
+            Dim coloursUsed() As Color
+            If Not IsNothing(colourNames) Then
+                'get colours
+                ReDim coloursUsed(UBound(colourNames))
+                For colourIndex As Integer = 0 To UBound(colourNames)
+                    coloursUsed(colourIndex) = ColorTranslator.FromHtml(colourNames(colourIndex))
+                Next
+
+                'gets pixels
+                If Not IsNothing(pixelsTag) Then
+                    Dim colourIndices(,) As Integer = pixelsTag.InterpretArgument
+
+                    For index2 As Integer = 0 To colourIndices.GetUpperBound(1)
+                        For index1 As Integer = 0 To colourIndices.GetUpperBound(0)
+                            Pixels(index1, index2) = coloursUsed(colourIndices(index1, index2))
+                        Next
+                    Next
+                Else
+                    PRE2.DisplayError("Unable to find tag for pixels in sprite " & fileName)
+                End If
+            Else
+                PRE2.DisplayError("Unable to find colours for sprite " & fileName)
+            End If
+        Else
+            PRE2.DisplayError("Unable to find tag for colours in sprite " & fileName)
+        End If
+    End Sub
 
     Public ReadOnly Property Bitmap As Image
         Get
+            If IsNothing(bitmapVersion) Then
+                bitmapVersion = ToBitmap()
+            End If
             Return bitmapVersion
         End Get
     End Property
@@ -92,11 +137,11 @@
         End Get
         Set(value As Color(,))
             pixelColours = value
-            bitmapVersion = ToBitmap()
+            bitmapVersion = Nothing
         End Set
     End Property
 
-    Public Function ToBitmap(Optional opacity As Single = 1) As Bitmap
+    Private Function ToBitmap(Optional opacity As Single = 1) As Bitmap
         'returns a bitmap version of this frame
 
         Dim result As New Bitmap(Pixels.GetUpperBound(0) + 1, Pixels.GetUpperBound(1) + 1, Imaging.PixelFormat.Format32bppArgb)
