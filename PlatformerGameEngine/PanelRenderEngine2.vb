583c69f56f6d007adb3a3bca46eeb8b03eb2b259
Richard Holmes <richard.gordon.holmes@gmail.com>
2019-11-26 20:59:52 +0000

    Layers sorted by insertion sort, added another render procedure which doesn't modify given actors list

--- PlatformerGameEngine/PanelRenderEngine2.vb
+++ PlatformerGameEngine/PanelRenderEngine2.vb
@@ -13,8 +13,10 @@ Public Class PanelRenderEngine2
 
 #Region "Rendering"
 
-    Public Sub DoGameRender(ByRef actorList() As Actor)
+    Public Sub DoGameRender(ByRef actorList As Actor())
         'renders a list of actors
+        'this uses double buffering to render the result to a buffer, which is eventually rendered to the user
+        'this elimates flickering caused by directly rendering one actor at a time
 
         Using canvas As New PaintEventArgs(renderPanel.CreateGraphics, New Rectangle(New Point(0, 0), renderPanel.Size))
             canvas.Graphics.InterpolationMode = Drawing2D.InterpolationMode.NearestNeighbor
@@ -22,24 +24,25 @@ Public Class PanelRenderEngine2
                 context.MaximumBuffer = renderPanel.Size
                 Using renderLayer As BufferedGraphics = context.Allocate(canvas.Graphics, canvas.ClipRectangle)
                     renderLayer.Graphics.InterpolationMode = Drawing2D.InterpolationMode.NearestNeighbor
+                    'sets the background colour to white
                     renderLayer.Graphics.FillRectangle(New SolidBrush(Color.White), New RectangleF(New PointF(0, 0), renderPanel.Size))
 
                     If Not IsNothing(actorList) Then
-                        'sorts actorList by layer ascending
-                        Dim swaps As Integer = 0
-                        Dim passes As Integer = 0
-                        Do While swaps > 0 Or passes = 0
-                            swaps = 0
-                            For index As Integer = 1 To UBound(actorList)
-                                If actorList(index).Layer < actorList(index - 1).Layer Then
-                                    Dim temp As Actor = actorList(index)
-                                    actorList(index) = actorList(index - 1)
-                                    actorList(index - 1) = temp
-                                    swaps += 1
-                                End If
-                            Next
-                            passes += 1
-                        Loop
+                        'sorts actorList by layer ascending using an insertion sort
+                        For i1 As Integer = 1 To UBound(actorList)
+                            If actorList(i1 - 1).Layer > actorList(i1).Layer Then
+                                'finds the index where the current actor should be
+                                Dim i2 As Integer = 0
+                                Do While actorList(i2).Layer < actorList(i1).Layer
+                                    i2 += 1
+                                Loop
+
+                                'moves the actor to its new index, shifting actors inbetween along
+                                Dim temp As Actor = actorList(i1)
+                                actorList = RemoveItem(actorList, i1)
+                                actorList = InsertItem(actorList, temp, i2)
+                            End If
+                        Next
 
                         'renders each actor in new sorted order
                         For actorIndex As Integer = 0 To UBound(actorList)
@@ -66,6 +69,13 @@ Public Class PanelRenderEngine2
         End Using
     End Sub
 
+    Public Sub DoGameRenderNoSort(ByVal actorList As Actor())
+        'renders the actor list, but the inputted actor list is not sorted like it is in the regular DoGameRender
+        'used in the level editor because it causes the actors to move around when layers are changed
+
+        DoGameRender(actorList)
+    End Sub
+
     Public ReadOnly Property RenderScale As SizeF   'the render scaling used by the renderer
         Get
             Return New SizeF(renderPanel.Size.Width / renderResolution.Width, renderPanel.Size.Height / renderResolution.Height)
@@ -74,212 +84,4 @@ Public Class PanelRenderEngine2
 
 #End Region
 
-
 End Class
-
-
-
-
-'Public Structure Frame
-'    'a frame of an actor is what is actually rendered, can be made up of multiple sprites with individual offsets
-
-'    Dim sprites() As Sprite
-'    Dim offsets() As Point
-
-'    Dim bitmapVersion As Bitmap
-'    Private spriteFolderLocation As String
-
-'    Public Sub New(frameTag As Tag, spriteFolderLocation As String)
-'        'creates a new frame from a frame tag
-
-'        Dim spriteTagStrings() As Object = frameTag.InterpretArgument()
-'        'ReDim sprites(UBound(spriteTagStrings))
-'        'ReDim offsets(UBound(spriteTagStrings))
-'        If Not IsNothing(spriteTagStrings) Then
-'            For index As Integer = 0 To UBound(spriteTagStrings)
-'                Dim spriteTag As New Tag(spriteTagStrings(index).ToString)
-'                Dim offsetArg As Object = spriteTag.InterpretArgument
-'                AddSprite(New Sprite(spriteFolderLocation & spriteTag.name), New Point(Val(offsetArg(0)), Val(offsetArg(1))))
-'            Next
-'        End If
-
-'        Me.spriteFolderLocation = spriteFolderLocation
-'    End Sub
-
-
-'    Public Function ToColourArray() As Color(,)
-'        Dim pixels(Dimensions.Width - 1, Dimensions.Height - 1) As Color
-
-'        For x As Integer = 0 To pixels.GetUpperBound(0)
-'            For y As Integer = 0 To pixels.GetUpperBound(1)
-'                pixels(x, y) = Color.Transparent            'sets all the default colours to transparent
-'            Next y
-'        Next x
-
-'        'this is the part which actually sets the frame's pixels
-'        For index As Integer = 0 To UBound(sprites)
-'            For x As Integer = offsets(index).X To offsets(index).X + sprites(index).pixels.GetUpperBound(0)
-'                For y As Integer = offsets(index).Y To offsets(index).Y + sprites(index).pixels.GetUpperBound(1)
-'                    If x >= 0 And y >= 0 Then       'checks that x and y are both positive
-'                        Dim pixelColour As Color = sprites(index).pixels(x - offsets(index).X, y - offsets(index).Y)
-
-'                        If pixelColour.A <> 0 Then
-'                            pixels(x, y) = pixelColour
-'                        End If
-'                    End If
-'                Next y
-'            Next x
-'        Next index
-
-'        Return pixels
-'    End Function
-
-'    Public Function ToBitmap(opacity As Single) As Bitmap
-'        'returns a bitmap version of this frame
-
-'        Dim pixels(,) As Color = ToColourArray()
-'        Dim result As New Bitmap(pixels.GetUpperBound(0) + 1, pixels.GetUpperBound(1) + 1, Imaging.PixelFormat.Format32bppArgb)
-'        result.MakeTransparent()
-
-'        For pixelY As Integer = 0 To pixels.GetUpperBound(1)
-'            For pixelX As Integer = 0 To pixels.GetUpperBound(0)
-'                'updates the alpha channel of the colour
-'                Dim pixelColour As Color = pixels(pixelX, pixelY)
-'                If pixelColour.A > 0 Then       'doesn't change pixels that are already transparent
-'                    pixelColour = Color.FromArgb(opacity * 255, pixelColour)
-'                End If
-
-'                result.SetPixel(pixelX, pixelY, pixelColour)
-'            Next pixelX
-'        Next pixelY
-
-'        Return result
-'    End Function
-
-'    Public Function Dimensions() As Size
-'        'returns the max X and Y of the frame
-
-'        Dim result As New Size(0, 0)
-'        For index As Integer = 0 To UBound(sprites)
-'            If Not IsNothing(sprites(index).pixels) Then
-'                Dim maxSize As New Size(sprites(index).pixels.GetLength(0) + offsets(index).X, sprites(index).pixels.GetLength(1) + offsets(index).Y)
-
-'                If maxSize.Width > result.Width Then
-'                    result.Width = maxSize.Width
-'                End If
-'                If maxSize.Height > result.Height Then
-'                    result.Height = maxSize.Height
-'                End If
-'            End If
-'        Next index
-
-'        Return result
-'    End Function
-
-'    Public ReadOnly Property Centre As PointF
-'        'returns the location of the centre of the frame
-
-'        Get
-'            Return New PointF(Dimensions.Width / 2, Dimensions.Height / 2)
-'        End Get
-'    End Property
-
-'    'Public Sub Trim()
-'    '    'removes any outermost rows or columns which only have transparent pixels in them
-'    '    'unfinished
-
-'    '    MsgBox("Trimming for frames isn't ready yet")
-'    '    Dim trimmedSides(3) As Boolean
-
-'    '    For side As Integer = 1 To trimmedSides.Length
-'    '        Do Until trimmedSides(side) = True
-'    '            Dim x As Integer = 0
-'    '            Dim y As Integer = 0
-
-'    '            If side = 1 Or side = 2 Then      'for 1 and 2: top and bottom
-'    '                Dim rowEmpty As Boolean = True
-'    '            ElseIf side = 3 Or side = 4 Then  'for 3 and 4: left and right
-'    '                Dim colEmpty As Boolean = True
-'    '            End If
-'    '        Loop
-'    '    Next
-'    'End Sub
-
-'    Public Shared Operator =(frame1 As Frame, frame2 As Frame)
-'        Return AreFramesEqual(frame1, frame2)
-'    End Operator
-
-'    Public Shared Operator <>(frame1 As Frame, frame2 As Frame)
-'        Return Not AreFramesEqual(frame1, frame2)
-'    End Operator
-
-'    Public Shared Function AreFramesEqual(frame1 As Frame, frame2 As Frame) As Boolean
-'        'returns whether 2 provided frames are identical
-
-'        Dim result As Boolean = True
-
-'        If frame1.Dimensions <> frame2.Dimensions Then      'checks that they are the same size
-'            result = False
-'        Else
-'            'checks that none of the pixels are different
-'            Dim pixels1(,) As Color = frame1.ToColourArray
-'            Dim pixels2(,) As Color = frame2.ToColourArray
-
-'            For x As Integer = 0 To frame1.Dimensions.Width - 1
-'                For y As Integer = 0 To frame1.Dimensions.Width - 1
-'                    If pixels1(x, y) <> pixels2(x, y) Then
-'                        result = False
-'                    End If
-'                Next y
-'            Next x
-'        End If
-
-'        Return result
-'    End Function
-
-'    Public Sub AddSprite(newSprite As Sprite, offset As Point)
-'        'adds a sprite to the frame
-
-'        If IsNothing(sprites) = True Then
-'            ReDim sprites(0)
-'        Else
-'            ReDim Preserve sprites(UBound(sprites) + 1)
-'        End If
-'        sprites(UBound(sprites)) = newSprite
-
-'        If IsNothing(offsets) = True Then
-'            ReDim offsets(0)
-'        Else
-'            ReDim Preserve offsets(UBound(offsets) + 1)
-'        End If
-'        offsets(UBound(offsets)) = offset
-'    End Sub
-
-'    Public Overrides Function ToString() As String
-'        If Not IsNothing(sprites) Then
-'            Dim spriteTags(UBound(sprites)) As Tag
-'            For index As Integer = 0 To UBound(sprites)
-'                spriteTags(index) = New Tag(sprites(index).fileName.Remove(0, Len(spriteFolderLocation)),
-'                                                ArrayToString({offsets(index).X, offsets(index).Y}))
-'            Next
-
-'            Return New Tag("frame", ArrayToString(spriteTags)).ToString
-'        Else
-'            Return New Tag("frame").ToString
-'        End If
-
-
-'        'Dim result As String = ""
-
-'        'For index As Integer = 0 To UBound(sprites)
-'        '    If IsNothing(sprites(index)) = False And Not IsNothing(offsets(index)) Then
-'        '        'this is wrong
-'        '        result += sprites(index).fileName.Remove(0, Len(spriteFolderLocation)) & "/" & offsets(index).ToString & ";"
-'        '    End If
-'        'Next
-
-'        'result = result.Remove(Len(result) - 1)
-
-'        'Return result
-'    End Function
-'End Structure
