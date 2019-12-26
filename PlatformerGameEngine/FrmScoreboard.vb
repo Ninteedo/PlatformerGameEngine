c709311b4e6521c963ddf8e549b412411632c5d4
Richard Holmes <richard.gordon.holmes@gmail.com>
2019-12-26 19:55:54 +0000

    Added a constructor which doesn't require a game name

--- PlatformerGameEngine/FrmScoreboard.vb
+++ PlatformerGameEngine/FrmScoreboard.vb
@@ -5,6 +5,25 @@ Public Class FrmScoreboard
 
 #Region "Constructors"
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        'gets the game name from the user
+
+        Dim input As String = InputBox("Enter the name of the game for which the scoreboard will be shown", "Enter Game Name", "Game Name")
+
+        'if cancel pressed then close the form
+        If IsNothing(input) Then
+            Close()
+        Else
+            GetScores(input)
+        End If
+    End Sub
+
     Public Sub New(gameName As String)
 
         ' This call is required by the designer.
@@ -24,40 +43,42 @@ Public Class FrmScoreboard
         'displays either top 10 scores or 10 scores around the search initials to the user
 
         Try
-            Dim conn As New MySqlConnection(Settings.ProjectScoresConnectionString)
-            conn.Open()
+            Using conn As New MySqlConnection(Settings.ProjectScoresConnectionString)
+                conn.Open()
 
-            Dim query As String =
+                Dim query As String =
                     "SELECT initials AS 'Player', MAX(points) AS 'Score'
                     FROM Score
                     WHERE gameName = @gameName
                     GROUP BY initials                    
                     ORDER BY MAX(points) DESC;"
 
-            'using preparing to prevent SQL injection attacks (injection possible for game name and possibly initials)
-            Using cmd As New MySqlCommand(query, conn)
-                'prepares query
-                cmd.Parameters.Add("@gameName", MySqlDbType.String, 64)
-                cmd.Prepare()
+                'uses preparing to prevent SQL injection attacks (injection possible for game name and possibly initials)
+                Using cmd As New MySqlCommand(query, conn)
+                    'prepares query
+                    cmd.Parameters.Add("@gameName", MySqlDbType.String, 64)
+                    cmd.Prepare()
 
-                'binds values to parameters
-                cmd.Parameters(0).Value = gameName
-                cmd.ExecuteNonQuery()
+                    'binds values to parameters
+                    cmd.Parameters(0).Value = gameName
+                    cmd.ExecuteNonQuery()
 
-                'executes and stores result in reader
-                Using reader As MySqlDataReader = cmd.ExecuteReader()
-
-                    If reader.HasRows Then
-                        DisplayScores(reader)
-                    Else
-                        DisplayError("This game has no scores associated with it")
-                    End If
+                    'executes and stores result in reader
+                    Using reader As MySqlDataReader = cmd.ExecuteReader()
+                        If reader.HasRows Then
+                            DisplayScores(reader)
+                        Else
+                            DisplayError("This game has no scores associated with it")
+                            Close()
+                        End If
+                    End Using
                 End Using
-            End Using
 
-            conn.Close()
+                conn.Close()
+            End Using
         Catch ex As Exception
             DisplayError("An Error has occured so score couldn't be displayed" & vbCrLf & ex.ToString())
+            Close()
         End Try
     End Sub
 
